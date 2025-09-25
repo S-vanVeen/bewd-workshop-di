8f67e44346f7017b3d7874bc976d36985df21005
boomboomSam <sam.veen2001@gmail.com>
2025-09-25 12:48:52 +0200

    uhhhhhh

--- src/main/java/nl/han/se/cnp/bewd/di/repository/MovieList.java
+++ src/main/java/nl/han/se/cnp/bewd/di/repository/MovieList.java
@@ -2,6 +2,7 @@ package nl.han.se.cnp.bewd.di.repository;
 
 import nl.han.se.cnp.bewd.di.domain.Movie;
 import nl.han.se.cnp.bewd.di.exceptions.MovieNotFoundException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -11,16 +12,21 @@ import java.util.List;
 public class MovieList {
     private List<Movie> movieList = new ArrayList<>();
 
-    public MovieList() {
+    public MovieList(
+            @Value("${movie1.title}") String movie1Title,
+            @Value("${movie2.title}") String movie2Title,
+            @Value("${movie3.title}") String movie3title
+    ) {
         Movie movie1 = new Movie();
         Movie movie2 = new Movie();
         Movie movie3 = new Movie();
+
         movie1.setId("1");
         movie2.setId("2");
         movie3.setId("3");
-        movie1.setName("Dune 2");
-        movie2.setName("Twisters");
-        movie3.setName("Love Actually");
+        movie1.setName(movie1Title);
+        movie2.setName(movie2Title);
+        movie3.setName(movie3title);
         movieList.add(movie1);
         movieList.add(movie2);
         movieList.add(movie3);
